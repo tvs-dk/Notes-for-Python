fcab7f21a0909fdb0327501a38d37e7386187866
tvs-dk <96741106+tvs-dk@users.noreply.github.com>
2023-03-03 13:21:39 +0100

    Create prompt.py

---
+++ prompt.py
@@ -0,0 +1,8 @@
+# Make a prompt asking a question
+
+answer = input("Answer this question? y/n ")
+
+# If the answer was y, gives the "answer" variable the value 1, otherwise give it 0
+if answer == "y": answer = 1
+else: answer = 0
+print(answer)
